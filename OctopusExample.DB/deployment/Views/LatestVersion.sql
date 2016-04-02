b8bb1a7844f12e4f827933e1717e958688632433
Kent Chenery <kent@chenery.net.nz>
2016-04-03 00:11:17 +1300

    Create a deployment.LatestVersion view

---
+++ OctopusExample.DB/deployment/Views/LatestVersion.sql
@@ -0,0 +1,34 @@
+﻿CREATE VIEW [deployment].[LatestVersion]
+AS
+/*
+ * Description:	Get the most recent and last successful deployments. These are determined by the IsSuccessfulDeployment
+ *				flag and the VersionID values.
+ */
+WITH MostRecentDeployments AS (
+	SELECT
+		'Most Recent' AS DeploymentType
+		,[VersionID], [VersionHierarchyID], [VersionNumber], [Major], [Minor], [Revision], [Build], 
+		[DeploymentVersion], [IsSuccessfulDeployment], [DeployedBy], [StartDateUTC], [EndDateUTC], [DurationMS]
+	FROM
+		deployment.[Version]
+	WHERE
+		VersionID = (SELECT MAX(VersionID) FROM deployment.Version)
+
+	UNION ALL 
+
+	SELECT
+		'Last Successful' AS DeploymentType
+		,[VersionID], [VersionHierarchyID], [VersionNumber], [Major], [Minor], [Revision], [Build], 
+		[DeploymentVersion], [IsSuccessfulDeployment], [DeployedBy], [StartDateUTC], [EndDateUTC], [DurationMS]
+	FROM
+		deployment.[Version]
+	WHERE
+		VersionID = (SELECT MAX(VersionID) FROM deployment.Version WHERE IsSuccessfulDeployment = 1)
+
+)
+SELECT
+	[DeploymentType], [VersionID], [VersionHierarchyID], [VersionNumber], [Major], [Minor], [Revision], [Build], 
+	[DeploymentVersion], [IsSuccessfulDeployment], [DeployedBy], [StartDateUTC], [EndDateUTC], [DurationMS]
+FROM
+	MostRecentDeployments
+;
